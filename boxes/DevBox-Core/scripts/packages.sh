8f930aa1481333f6f907e9f2a5d72a967ff7346b
Jake Woods <vodurden@gmail.com>
2015-09-17 20:25:17 +1000

    Add firefox

--- boxes/DevBox-Core/scripts/packages.sh
+++ boxes/DevBox-Core/scripts/packages.sh
@@ -15,10 +15,10 @@ pacman-key --lsign-key 962DDE58
 pacman -Syy
 
 # Fontconfig and Freetype2 will be replaced by their infinality equivalents
-yes | pacman -S cairo-infinality-ultimate 
-yes | pacman -S fontconfig-infinality-ultimate 
-yes | pacman -S freetype2-infinality-ultimate 
-yes | pacman -S ibfonts-meta-extended
+yes | pacman -S cairo-infinality-ultimate
+yes | pacman -S fontconfig-infinality-ultimate
+yes | pacman -S freetype2-infinality-ultimate
+yes | pacman -S ibfonts-meta-base
 
 # X Core
 pacman --noconfirm -S \
@@ -31,6 +31,9 @@ pacman --noconfirm -S \
        rxvt-unicode \
        xmonad \
        xmonad-contrib
+
+pacman --noconfirm -S \
+       firefox
 
 pacman --noconfirm -S \
        vim \
