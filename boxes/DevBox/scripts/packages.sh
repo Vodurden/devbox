e6382c1dd82e54555664bb658d166c5506d77886
Jake Woods <vodurden@gmail.com>
2015-10-28 20:05:15 +1100

    Add dialyzer support to elixir mix

--- boxes/DevBox/scripts/packages.sh
+++ boxes/DevBox/scripts/packages.sh
@@ -21,6 +21,14 @@ pacman --noconfirm -S \
        rust \
        markdown
 
+# Install dialyxir so we can statically analyse elixir!
+git clone https://github.com/jeremyjh/dialyxir.git /tmp/dialyxir \
+    && cd /tmp/dialyxir \
+    && mix archive.build \
+    && mix archive.install \
+    && mix dialyzer.plt \
+    && rm -rf /tmp/dialyxir
+
 # Install node packages for spacemacs javascript support
 npm install -g tern js-beautify
 
