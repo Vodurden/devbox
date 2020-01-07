b7fbfa4245d5cdd4149427e294623db0952ca0b8
Jake Woods <jake@jakewoods.net>
2020-01-07 15:06:43 +1100

    Update rea-as to 1.5.3

--- nixpkgs/rea/rea-as/default.nix
+++ nixpkgs/rea/rea-as/default.nix
@@ -1,19 +1,21 @@
-{ stdenv, fetchgit, buildGoPackage }:
+{ stdenv, fetchgit, buildGoModule }:
 
-buildGoPackage rec {
-  version = "1.3.4";
+buildGoModule rec {
+  version = "1.5.3";
   baseName = "rea-as";
   name = "${baseName}-${version}";
 
   goPackagePath = "git.realestate.com.au/cowbell/rea-as";
 
+  modSha256 = "19jjsakglqdx6sppfjjgypgngvk51sxbj6fdzhk028mb7nj9wkmj";
+
   # Assumes we have run `nix-prefetch-git` as the source is private:
   #
-  #     nix-prefetch-git git@git.realestate.com.au:cowbell/rea-as.git e76e1cbfe7b8010f845e51379d3bbcd889dfe6c8 10ipnni2b68y36bxjnmy8y6jrrm26n1q84kl00pzsb19v4zjn9f7
+  #     nix-prefetch-git git@git.realestate.com.au:cowbell/rea-as.git 88337bfdf733248b533a945648cb5cbe5a07299e 1n8plr45ggyjyv0n509rz59c284hw75nzyf42j5bc2z6lp8gd8ln
   #
   src = fetchgit {
     url = "git@git.realestate.com.au:cowbell/rea-as.git";
-    rev = "e76e1cbfe7b8010f845e51379d3bbcd889dfe6c8";
-    sha256 = "10ipnni2b68y36bxjnmy8y6jrrm26n1q84kl00pzsb19v4zjn9f7";
+    rev = "88337bfdf733248b533a945648cb5cbe5a07299e";
+    sha256 = "1n8plr45ggyjyv0n509rz59c284hw75nzyf42j5bc2z6lp8gd8ln";
   };
 }
