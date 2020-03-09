c081446d6718c6749ab47c1c545f7639543bff03
Jake Woods <jake@jakewoods.net>
2020-03-09 13:09:46 +1100

    Automatically add new modules to programs, games and languages
    After this we need to centralise the duplicated functions

--- config/modules/games/default.nix
+++ config/modules/games/default.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ lib, ... }:
+
+let
+  folder = ./.;
+
+  importNixIf = key: value: key != "default.nix" && value == "regular" && lib.hasSuffix ".nix" key;
+  importDirectoryIf = key: value: value == "directory" && builtins.pathExists (./. + key + ./default.nix);
+  importIf = key: value: (importNixIf key value) || (importDirectoryIf key value);
+
+  toImport = name: value: folder + ("/" + name);
+  imports = lib.mapAttrsToList toImport (lib.filterAttrs importIf (builtins.readDir folder));
+in
 
 {
-  imports = [
-    ./minecraft.nix
-    ./steam.nix
-  ];
+  inherit imports;
 }
