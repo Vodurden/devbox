0135451096d40360fe738b2ced7817f1c2196bbc
Jake Woods <jake@jakewoods.net>
2020-03-24 12:17:36 +1100

    Add workaround for ssh issue in DataGrip

--- config/modules/programs/ssh.nix
+++ config/modules/programs/ssh.nix
@@ -10,5 +10,13 @@
         identityFile = "~/.ssh/id_rsa";
       };
     };
+
+    # Workaround for "Unable to reach a settlement: [ssh-dss] and [ssh-rsa, rsa-sha2-512, rsa-sha2-256, ssh-ed25519]" in DataGrip
+    #
+    # See also: https://github.com/NixOS/nixpkgs/issues/13393#issuecomment-534846475 and https://superuser.com/a/1485062
+    extraConfig = ''
+      Host *
+        HostKeyAlgorithms rsa-sha2-512,rsa-sha2-256,ssh-rsa,ssh-ed25519
+    '';
   };
 }
