af41189d93733e9d8c70b8171c5ab069d6872ad9
Jake Woods <jake@jakewoods.net>
2020-09-04 13:53:58 +1000

    Fix scrolling to be natural on touchpad but normal on mousewheel

--- config/modules/hardware/touchpad.nix
+++ config/modules/hardware/touchpad.nix
@@ -7,5 +7,10 @@
     naturalScrolling = true;     # Up is down, down is up!
     accelSpeed = "0.250000";
     accelProfile = "flat";
+
+    # Only apply libinput settings to the touchpad
+    #
+    # See: https://github.com/NixOS/nixpkgs/issues/75007#issuecomment-617577348
+    additionalOptions = ''MatchIsTouchpad "on"'';
   };
 }
