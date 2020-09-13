b9704e9257b462c1c16bfcf3411d2ec7f1dc8bb1
Jake Woods <jake@jakewoods.net>
2020-09-13 18:00:18 +1000

    Auto-start tccd

--- nix/modules/nixos/tuxedo-control-center.nix
+++ nix/modules/nixos/tuxedo-control-center.nix
@@ -21,5 +21,41 @@ in
     boot.kernelModules = [ "tuxedo-cc-wmi "];
 
     services.dbus.packages = [ pkgs.tuxedo-control-center ];
+
+    systemd.services.tccd = {
+      path = [ pkgs.tuxedo-control-center ];
+
+      description = "TUXEDO Control Center Service";
+
+      # Apply undervolt on boot, nixos generation switch and resume
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.tuxedo-control-center}/bin/tccd --start";
+        ExecStop = "${pkgs.tuxedo-control-center}/bin/tccd --stop";
+      };
+    };
+
+    systemd.services.tccd-sleep = {
+      path = [ pkgs.tuxedo-control-center ];
+
+      description = "TUXEDO Control Center Service (sleep/resume)";
+
+      # Apply undervolt on boot, nixos generation switch and resume
+      wantedBy = [ "sleep.target" ];
+
+      unitConfig = {
+        StopWhenUnneeded="yes";
+      };
+
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit="yes";
+        # TODO: Do we need bash -c "systemctl stop tccd" here instead?
+        ExecStart = "systemctl stop tccd";
+        ExecStop = "systemctl start tccd";
+      };
+    };
   };
 }
