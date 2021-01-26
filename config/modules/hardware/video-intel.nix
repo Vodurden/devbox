9bb575c0b9e0cc26b532235a9774ab0ff78a0e32
Jake Woods <jake.woods@rea-group.com>
2021-01-26 23:31:30 +1100

    Better intel hardware acceleration
    Now steam will open instead of crashing immediately

--- config/modules/hardware/video-intel.nix
+++ config/modules/hardware/video-intel.nix
@@ -3,7 +3,16 @@
 {
   services.xserver.videoDrivers = ["intel"];
   services.xserver.deviceSection = ''
-    Option "DRI" "2"
+    Option "DRI" "3"
     Option "TearFree" "true"
   '';
+
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages = [
+    pkgs.vaapiIntel
+  ];
+
+  primary-user.extraGroups = [ "video" ];
 }
