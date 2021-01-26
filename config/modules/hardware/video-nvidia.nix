db2e66eb404a8b582e6a5adb9e0ae028d0e9c0c6
Jake Woods <jake@jakewoods.net>
2021-01-26 18:04:01 +1100

    Nvidia tweaks

--- config/modules/hardware/video-nvidia.nix
+++ config/modules/hardware/video-nvidia.nix
@@ -4,6 +4,7 @@
   hardware.opengl.enable = true;
   hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
+  hardware.nvidia.modesetting.enable = true;
 
   services.xserver.videoDrivers = ["nvidia"];
 
@@ -17,8 +18,14 @@
   # - Fast but stuck on max power:
   #   Option "RegistryDwords" "PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerDefault=0x1; PowerMizerDefaultAC=0x1"
   #
+  # - Scalable:
+  #   Option "RegistryDwords" "PowerMizerEnable=0x1; PerfLevelSrc=0x3333"
+  #
+  # - Stuck on 3/4 power:
+  #   Option "RegistryDwords" "PowerMizerEnable=0x1; PerfLevelSrc=0x3333; OverrideMaxPerf=0x4"
   services.xserver.deviceSection = ''
     Option "RegistryDwords" "PowerMizerEnable=0x1; PerfLevelSrc=0x3333"
+    # Option "RegistryDwords" "PowerMizerEnable=0x1; PerfLevelSrc=0x3333; OverrideMaxPerf=0x4"
     Option "Coolbits" "31"
     Option "UseNvKmsCompositionPipeline" "Off"
     Option "TripleBuffer" "On"
