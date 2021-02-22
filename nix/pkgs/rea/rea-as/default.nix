c136152119c24810d77168e4a9cdda59f99eb760
Jake Woods <jake@jakewoods.net>
2021-02-22 12:38:18 +1100

    Update `rea-as`

--- nix/pkgs/rea/rea-as/default.nix
+++ nix/pkgs/rea/rea-as/default.nix
@@ -1,18 +1,22 @@
 { stdenv, buildGoModule }:
 
 buildGoModule rec {
-  version = "2.0.4";
+  version = "2.0.6";
   baseName = "rea-as";
   name = "${baseName}-${version}";
 
   goPackagePath = "git.realestate.com.au/cowbell/rea-as";
 
-  vendorSha256 = "1jxda5lqdhq1fvh58fmfjm2g61ic31zqyk4ihssd76ni15328pc0";
+  vendorSha256 = "121b1sm8a3kn77ifk89dcsbd5rarwfah318184rwfx5cc929xzrp";
 
-  # Private modules work best with builtins.fetchGit instead of fetchgit. Make sure to use the `ssh://` style as it seems
-  # to be the only one that works with your user's ssh config
+  # Private modules work best with builtins.fetchGit instead of fetchgit.
+  # Make sure to use the `ssh://` style as it seems to be the only one that
+  # works with your user's ssh config
+  #
+  # Note: When updating, first run `nixos-rebuild build` then `sudo -E nixos-rebuild switch`
   src = builtins.fetchGit {
     url = ssh://git@git.realestate.com.au/cowbell/rea-as.git;
-    rev = "eba0aeeb840f71eb799beb905f1d42b16a2744cf"; # v2.0.4
+    ref = "v2.0.6";
+    rev = "44c498cdcbd811235dd3a005cbf103f14e56b4fb"; # v2.0.6
   };
 }
