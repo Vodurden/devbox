13eeec90ea4d5b779ecda10241762f544dc60735
Jake Woods <jake@jakewoods.net>
2021-05-02 12:39:59 +1000

    Remove loop hero patches
    New steam is no longer broken

--- games/loop-hero/run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-nix-shell -p '(steam.override { extraLibraries = (pkgs: [ pkgs.openssl pkgs.nghttp2 pkgs.libidn2 pkgs.rtmpdump pkgs.libpsl pkgs.curl pkgs.krb5 pkgs.keyutils ]); }).run' --command "steam-run ./Loop_Hero"
