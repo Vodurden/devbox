7a753af9b057bea37bc789bcc25c153448066ad1
Jake Woods <jake@jakewoods.net>
2021-07-15 17:54:19 +1000

    Apply steam fix from main branch

--- shell.nix
+++ shell.nix
@@ -11,9 +11,21 @@ in
         pkgs.lib.mapAttrsToList (k: v: "${k}=${v}") path
       );
 
+    nixpkgs-unstable-patched = applyPatches {
+      name = "nixpkgs-unstable-patched";
+      src = sources.nixpkgs-unstable;
+      patches = [
+        # Fix steam proton: https://github.com/NixOS/nixpkgs/pull/129805
+        (fetchpatch {
+          url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/129805.diff";
+          sha256 = "0aq8cjjh84wg5sj0zi7ppsi5r2akh6w3zphqjglywzs0mhll6yyj";
+        })
+      ];
+    };
+
     # Make sure to update ./config/modules/nix/nix-path.nix if changing this setting
     nix-path = build-nix-path-env-var {
-      nixpkgs = sources.nixpkgs-unstable;
+      nixpkgs = nixpkgs-unstable-patched;
       nixpkgs-unstable = sources.nixpkgs-unstable;
       nixpkgs-stable = sources.nixpkgs-stable;
       nixpkgs-master = sources.nixpkgs-master;
