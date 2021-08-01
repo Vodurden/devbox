6b1db7ab43565d271aaf7efaa61d3c40e6a6b690
Jake Woods <jake@jakewoods.net>
2021-08-01 14:20:53 +1000

    Remove unecessary linebreak

--- config/modules/services/duplicati.nix
+++ config/modules/services/duplicati.nix
@@ -6,8 +6,7 @@
     user = config.primary-user.name;
   };
 
-  # Type=Link desktop items don't seem to show up in Gnome 40, so let's work around it
-  # by using Exec
+  # Type=Link desktop items don't seem to show up in Gnome 40, so let's work around it by using Exec
   primary-user.home-manager.xdg.desktopEntries.duplicati = {
     name = "Duplicati";
     exec = "firefox 127.0.0.1:8200";
