f06f565286e069ec5c58c222c97e37f0f7c5b551
Jake Woods <jake@jakewoods.net>
2021-08-02 01:01:58 +1000

    Fix ntlm_auth in wine

--- config/modules/games/wine.nix
+++ config/modules/games/wine.nix
@@ -1,6 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
+  # We need samba for ntlm_auth
+  services.samba = {
+    enable = true;
+    enableWinbindd = true;
+  };
+
   primary-user.home-manager = {
     home.packages = [
       pkgs.wineWowPackages.full
