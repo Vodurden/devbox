ccce6b8db0e4f9fb6753dc94b9def67a7d97209f
Jake Woods <jake@jakewoods.net>
2022-08-21 15:51:16 +1000

    Install expressvpn

--- config/nixos/hardware/network.nix
+++ config/nixos/hardware/network.nix
@@ -5,6 +5,8 @@
 
   networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
+  services.expressvpn.enable = true;
+
   environment.systemPackages = with pkgs; [
     networkmanagerapplet
 
@@ -12,6 +14,7 @@
     openconnect
     openssl
     gnome3.networkmanager-openconnect
+    expressvpn
   ];
 
   primary-user.extraGroups = [ "networkmanager" ];
