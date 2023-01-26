129a84e1b6b1547ee39717261ef024017bea356c
Jake Woods <jake@jakewoods.net>
2023-01-26 13:23:01 +1100

    Add burn-my-windows effect to gnome

--- config/nixos/ui/gnome/dconf.nix
+++ config/nixos/ui/gnome/dconf.nix
@@ -1,6 +1,7 @@
 { lib, ... }:
 
 {
+  # Use `dconf watch /` and edit the setting to see what changes
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       enable-hot-corners = false;
--- config/nixos/ui/gnome/default.nix
+++ config/nixos/ui/gnome/default.nix
@@ -1,5 +1,31 @@
 { config, pkgs, ... }:
 
+let
+  gnomeExtensions = [
+    {
+      package = pkgs.gnomeExtensions.burn-my-windows;
+      dconf = {
+        apparition-open-effect = true;
+        apparition-animation-time = 300;
+
+        broken-glass-close-effect = true;
+        broken-glass-animation-time = 1000;
+        broken-glass-scale = 0.55;
+
+        # Disable the defaults
+        fire-close-effect = false;
+        glide-close-effect = false;
+      };
+    }
+  ];
+
+  gnomeExtensionsDconf = pkgs.lib.mkMerge (map (ext:
+    {
+      "org/gnome/shell/extensions/${ext.package.extensionPortalSlug}" = ext.dconf;
+    }
+  ) gnomeExtensions);
+in
+
 {
   services.xserver = {
     enable = true;
@@ -27,6 +53,14 @@
     home.packages = [
       pkgs.gnome3.dconf-editor
       pkgs.dconf2nix
-    ];
+    ] ++ map (ext: ext.package) gnomeExtensions;
+
+    # Enable our gnome extensions
+    dconf.settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = map (ext: ext.package.extensionUuid) gnomeExtensions;
+      };
+    } // gnomeExtensionsDconf;
   };
 }
