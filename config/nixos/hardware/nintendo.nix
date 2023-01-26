b55f3f3c7bf45bc5003a56fd3c82686bc2598314
Jake Woods <jake@jakewoods.net>
2023-01-26 13:21:44 +1100

    Fix Nintendo Switch Pro controller desync
    Turns out hid-nintendo is now older then the inline package, we can just
    rely on joycond choosing the appropriate module if necessary (or just
    rely on the kernel sicne hid-nintendo is now merged)

--- config/nixos/hardware/nintendo.nix
+++ config/nixos/hardware/nintendo.nix
@@ -1,12 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = [
-    pkgs.kernelPackages.hid-nintendo
-  ];
-
-  boot.extraModulePackages = [ pkgs.kernelPackages.hid-nintendo ];
-  boot.kernelModules = [ "hid_nintendo" ];
-
   services.joycond.enable = true;
 }
