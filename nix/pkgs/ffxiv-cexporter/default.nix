f9c0e933564fc5ab10089090ddaa3667d0e83521
Jake Woods <jake@jakewoods.net>
2024-08-10 22:36:00 +1000

    ffxiv-cexporter update

--- nix/pkgs/ffxiv-cexporter/default.nix
+++ nix/pkgs/ffxiv-cexporter/default.nix
@@ -1,23 +1,26 @@
-{ lib, buildDotnetModule, fetchFromGitHub, dotnetCorePackages }:
+{ pkgs, buildDotnetModule, fetchFromGitHub, dotnetCorePackages }:
 
 buildDotnetModule rec {
   pname = "FFXIVClientStructs-CExporter";
   version = "1.0.0";
 
-  src = fetchFromGitHub {
-    owner = "aers";
-    repo = "FFXIVClientStructs";
-    rev = "f84ba85e87976b711c1ff4795da0a5217cd9dc1f";
-    sha256 = "sha256-K8oY9DD7w9Y/2lQFcFpqz8GkPcdGedRT3JrB8ybxq+I=";
+  src = pkgs.fetchgit {
+    url = "https://github.com/aers/FFXIVClientStructs.git";
+    sha256 = "sha256-DEXsrtY0LXw3kxGeHhIyLL/zvkkMqiYcvazuQ22b4f0=";
+
+    # We need to use leaveDotGit because FFXIVClientStructs uses GitInfo which breaks without .git
+    leaveDotGit = true;
   };
+
+  buildInputs = [
+    pkgs.git
+  ];
 
   # Generated with `nix-build -E "with import <nixpkgs> {}; (callPackage ./default.nix {}).passthru.fetch-deps"`
   nugetDeps = ./ffxiv-cexporter-deps.nix;
 
-  dotnet-sdk = dotnetCorePackages.sdk_7_0;
-  dotnet-runtime = dotnetCorePackages.runtime_7_0;
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
-  projectFile = [
-    "ida/CExporter/CExporter.csproj"
-  ];
+  projectFile = "./CExporter/CExporter.csproj";
 }
